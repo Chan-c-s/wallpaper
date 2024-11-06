596bb1b01be55cc8b6c4fa709d0dcb58523b6d96
Chan-c-s <110450830+Chan-c-s@users.noreply.github.com>
2024-11-06 15:29:28 +0800

    Add files via upload

---
+++ InstallLocalWordpress.ps1
@@ -0,0 +1,8 @@
+﻿# Define path to the installer
+$installerPath = "C:\Wordpress\local-9.1.0-windows.exe"
+
+# Define silent installation arguments
+$silentArgs = "/quiet /norestart /ALLUSERS"
+
+# Run the EXE installer silently
+Start-Process -FilePath $installerPath -ArgumentList $silentArgs -Wait -NoNewWindow
